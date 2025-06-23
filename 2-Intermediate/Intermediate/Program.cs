b0941a92d98f47c8b462ac0f5601a8ab6b3e3a49
Jesse <196892153+Hjesse25@users.noreply.github.com>
2025-06-22 17:45:24 -0700

    lesson dapper

--- 2-Intermediate/Intermediate/Program.cs
+++ 2-Intermediate/Intermediate/Program.cs
@@ -1,4 +1,11 @@
-﻿using Intermediate.Models;
+﻿using Intermediate.Data;
+using Intermediate.Models;
+
+DataContextDapper dapper = new DataContextDapper();
+
+DateTime rightNow = dapper.LoadDataSingle<DateTime>("SELECT GETDATE()");
+
+// Console.WriteLine(rightNow.ToString());
 
 Computer myComputer = new Computer()
 {
@@ -10,7 +17,53 @@ Computer myComputer = new Computer()
     VideoCard = "RTX 2060"
 };
 
-myComputer.HasWifi = false;
-Console.WriteLine(myComputer.Motherboard);
-Console.WriteLine(myComputer.HasWifi);
-Console.WriteLine(myComputer.VideoCard);
+string sql = @"INSERT INTO TutorialAppSchema.Computer (
+    Motherboard,
+    HasWifi,
+    HasLTE,
+    ReleaseDate,
+    Price,
+    VideoCard
+) VALUES ('" + myComputer.Motherboard
+        + "','" + myComputer.HasWifi
+        + "','" + myComputer.HasLTE
+        + "','" + myComputer.ReleaseDate.ToString("MM-dd-yyyy")
+        + "','" + myComputer.Price
+        + "','" + myComputer.VideoCard
++ "')";
+
+// Console.WriteLine(sql);
+
+// int result = dapper.ExecuteSqlWithRowCount(sql);
+bool result = dapper.ExecuteSql(sql);
+
+// Console.WriteLine(result);
+
+string sqlSelect = @"SELECT 
+    Computer.Motherboard,
+    Computer.HasWifi,
+    Computer.HasLTE,
+    Computer.ReleaseDate,
+    Computer.Price,
+    Computer.VideoCard
+FROM TutorialAppSchema.Computer";
+
+IEnumerable<Computer> computers = dapper.LoadData<Computer>(sqlSelect);
+
+Console.WriteLine("'Motherboard', 'HasWifi', 'HasLTE', 'ReleaseDate'"
+    + "'Price', 'VideoCard'");
+foreach (Computer singleComputer in computers)
+{
+    Console.WriteLine(myComputer.Motherboard
+        + "','" + myComputer.HasWifi
+        + "','" + myComputer.HasLTE
+        + "','" + myComputer.ReleaseDate.ToString("MM-dd-yyyy")
+        + "','" + myComputer.Price
+        + "','" + myComputer.VideoCard
++ "'");
+}
+
+// myComputer.HasWifi = false;
+// Console.WriteLine(myComputer.Motherboard);
+// Console.WriteLine(myComputer.HasWifi);
+// Console.WriteLine(myComputer.VideoCard);
